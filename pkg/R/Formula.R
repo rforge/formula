56b8b388c03dd1cd3354809d8bef10c75c75fcab
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-25 16:40:16 +0000

    added all.equal() method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/formula@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Formula.R
+++ pkg/R/Formula.R
@@ -236,7 +236,25 @@ print.Formula <- function(x, ...) {
   invisible(x)
 }
 
+all.equal.Formula <- function(target, current, ...) {
+  rval <- NULL
+  
+  if(length(target)[1] != length(current)[1]) {
+    rval <- c(rval, paste("Length mismatch: target, current differ in number of LHS parts: ",
+      length(target)[1], ", ", length(current)[1], sep = ""))
+  } else if(!isTRUE(all.equal(attr(target, "lhs"), attr(current, "lhs")))) {
+    rval <- c(rval, "Formula mismatch: LHS formulas differ in contents")
+  }
 
+  if(length(target)[2] != length(current)[2]) {
+    rval <- c(rval, paste("Length mismatch: target, current differ in number of RHS parts: ",
+      length(target)[2], ", ", length(current)[2], sep = ""))
+  } else if(!isTRUE(all.equal(attr(target, "rhs"), attr(current, "rhs")))) {
+    rval <- c(rval, "Formula mismatch: RHS formulas differ in contents")
+  }
+  
+  if(is.null(rval)) TRUE else rval
+}
 
 ## convenience tools #################################################
 
