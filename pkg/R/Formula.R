6e2e6485d7ead9e506d5efe170e0f06f671ac7e9
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-07-12 12:50:40 +0000

    added str() method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/formula@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Formula.R
+++ pkg/R/Formula.R
@@ -266,6 +266,16 @@ all.equal.Formula <- function(target, current, ...) {
   if(is.null(rval)) TRUE else rval
 }
 
+str.Formula <- function(object, ...) {
+  le <- length(object)
+  ls <- ifelse(sum(le) > 1L, "s", "")
+  writeLines(c(
+    sprintf("'Formula' with %s left-hand and %s right-hand side%s: %s",
+      le[1L], le[2L], ls, format(object)),
+    sprintf("  ..- attr(*, \".Environment\")=%s", format(attr(object, ".Environment")))))
+  invisible()
+}
+
 ## convenience tools #################################################
 
 ## split formulas
